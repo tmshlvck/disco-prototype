2a7dd050727fd5d0d4585b14fafdeeac200aa41a
Yossi Gilad <yossig2@gmail.com>
2017-11-23 08:42:20 +0100

    Add crypto Ownership Token generation
    Add function to generate cryptographic keys for use in Ownership Tokens.

--- attrannounce.py
+++ attrannounce.py
@@ -2,21 +2,30 @@
 
 from sys import stdout
 from time import sleep
+import donna25519
 
 
 prefixes = ['192.168.0.0/24',]
 
 
-def gen_key_attr(prefix):
+
+
+def gen_key_attr():
     # TODO: generate the crypto-based value
-    return '0x'+('0010000000000001'*4) # return static example now - 32 bytes
+    private_key = donna25519.PrivateKey()
+    priv_key_str = private_key.private
+    pub_key_str = private_key.get_public().public
+    return priv_key_str, pub_key_str
+
+
+#    return '0x'+('0010000000000001'*4) # return static example now - 32 bytes
 
 def main():
     sleep(5)
-
     message = 'announce route %s next-hop self attribute [0x40 0xe0 %s] \n'
     for p in prefixes:
-        stdout.write(message % (p, gen_key_attr(p)))
+        private_key, public_key = gen_key_attr()
+        stdout.write(message % (p, public_key))
         stdout.flush()
         sleep(1)
 
